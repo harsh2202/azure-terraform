0f4e34d5b0a8df9a9d6988f19b18847bfd1e51e8
Harish Kumar <harish.h.kumar@mercedes-benz.com>
2024-03-09 10:39:06 +0100

    update

--- modules/infra/storage/main.tf
+++ modules/infra/storage/main.tf
@@ -11,10 +11,30 @@ provider "azurerm" {
   features {}
 }
 
-
 # Creating the Storage account
 
 resource "azurerm_resource_group" "common_RG" {
     name = var.rgName  
     location = var.location
+}
+
+resource "random_string" "rstr" {
+  length = 8
+  special = true
+  upper = true
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                      = "${var.rgName}${random_string.rstr.result}"
+  resource_group_name       = azurerm_resource_group.common_RG.name
+  location                 = var.location   
+  account_tier             = "Standard"
+  account_replication_type  = "LRS"
+  
+}
+
+resource "azurerm_storage_container" "statefile" {
+  name                 = "statefiles"
+  storage_account_name = azurerm_storage_account.storage.name
+   
 }
