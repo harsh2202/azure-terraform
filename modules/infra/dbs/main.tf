401484f237272002229ffa76a136b9d46c8eef4c
Harish Kumar <harish.h.kumar@mercedes-benz.com>
2024-03-13 14:30:42 +0100

    update

--- modules/infra/dbs/main.tf
+++ modules/infra/dbs/main.tf
@@ -1,40 +1,30 @@
-# Azure provider version 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.0"
-    }
-  }
-}
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "this" {
-  name     = var.resource_group_name
-  location = var.location
+data "azurerm_resource_group" "rgname" {
+  name = "DefaultResourceGroup-EUS"
 }
 
-# Virtual Network for private endpoint 
-resource "azurerm_virtual_network" "this" {
-  name                = var.virtual_network_name
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  address_space       = var.vnet_address_space
-}
+# # Virtual Network for private endpoint 
+# resource "azurerm_virtual_network" "this" {
+#   name                = var.virtual_network_name
+#   resource_group_name = azurerm_resource_group.this.name
+#   location            = azurerm_resource_group.this.location
+#   address_space       = var.vnet_address_space
+# }
 
-resource "azurerm_subnet" "this" {
-  name                                           = var.subnet_name
-  resource_group_name                            = azurerm_resource_group.this.name
-  virtual_network_name                           = azurerm_virtual_network.this.name
-  address_prefixes                               = var.subnet_prefixes
-  enforce_private_link_endpoint_network_policies = true
-}
+# resource "azurerm_subnet" "this" {
+#   name                                           = var.subnet_name
+#   resource_group_name                            = azurerm_resource_group.this.name
+#   virtual_network_name                           = azurerm_virtual_network.this.name
+#   address_prefixes                               = var.subnet_prefixes
+#   enforce_private_link_endpoint_network_policies = true
+# }
 
 # Private DNS Zone for SQL API 
-resource "azurerm_private_dns_zone" "this" {
+resource "azurerm_private_dns_zone" "dns_zone" {
   name                = "privatelink.documents.azure.com"
   resource_group_name = azurerm_resource_group.this.name
 }
@@ -42,7 +32,7 @@ resource "azurerm_private_dns_zone" "this" {
 resource "azurerm_private_dns_zone_virtual_network_link" "this" {
   name                  = var.private_dns_vnet_link_name
   resource_group_name   = azurerm_resource_group.this.name
-  private_dns_zone_name = azurerm_private_dns_zone.this.name
+  private_dns_zone_name = azurerm_private_dns_zone.dns_zone.name
   virtual_network_id    = azurerm_virtual_network.this.id
   registration_enabled  = false
 }
@@ -50,7 +40,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "this" {
 module "azure_cosmos_db" {
   source              = "Azure/cosmosdb/azurerm"
   resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
+  location            = data.azurerm_resource_group.rgname.location
   cosmos_account_name = var.cosmos_account_name
   cosmos_api          = var.cosmos_api
   sql_dbs             = var.sql_dbs
@@ -58,7 +48,7 @@ module "azure_cosmos_db" {
   private_endpoint = {
     "pe_endpoint" = {
       dns_zone_group_name             = var.dns_zone_group_name
-      dns_zone_rg_name                = azurerm_private_dns_zone.this.resource_group_name
+      dns_zone_rg_name                = data.azurerm_private_dns_zone.rgname.resource_group_name
       is_manual_connection            = false
       name                            = var.pe_name
       private_service_connection_name = var.pe_connection_name
